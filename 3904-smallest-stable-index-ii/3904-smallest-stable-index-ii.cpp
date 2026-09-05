ec8594a80dda2f1b070faa531e589cfc58ba7041
Berwin CR <berwincr4028@gmail.com>
2026-09-05 22:23:13 +0530

    Time: 11 ms (57.63%), Space: 210.9 MB (15.41%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
        int n = nums.size();
+       
         vector<int>PrefixMax(n);
         vector<int>SuffixMin(n);
-       int min_idx=INT_MAX;
+
+     
         PrefixMax[0]= nums[0];
         SuffixMin[n-1] = nums[n-1];
-        int min_element = INT_MAX;
-        int max_element = INT_MIN;
-
+       
         for(int i=1;i<n;i++){
                PrefixMax[i]= max(nums[i], PrefixMax[i-1]);
         }
