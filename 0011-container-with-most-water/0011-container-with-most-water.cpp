2842d1cd4962431f2a958a2c550e8cdb07ba199c
Berwin CR <berwincr4028@gmail.com>
2026-09-16 23:42:02 +0530

    Time: 0 ms (100%), Space: 63 MB (10.91%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -7,7 +7,7 @@ int water=0;
 int left=0;
 int right=height.size()-1;
         while(left<=right){
-            if(height[left]<height[right]){
+            if(height[left]<height[right]){              //Always choose the smallest height
                water = height[left];
                area = max(area, water*(right-left));
                left++;
